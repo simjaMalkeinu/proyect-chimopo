1a8aff41dbb2d3714ccb7d49bb6d8a3a1e69ea82
simja <yorkammalkeinu@gmail.com>
2021-01-20 09:14:23 -0600

    last proyect

--- src/components/Resumenes.jsx
+++ src/components/Resumenes.jsx
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
-import Data from '../json/resumenes.json'
-import Buscador from './Buscador';
-import Temas from './Temas';
+import React, { Component } from "react";
+import Data from "../json/resumenes.json"; // datos del JSON
+import Buscador from "./Buscador";
+import Temas from "./Temas";
 
 export default class Resumenes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filtro: "",
+      buscador: "",
+    };
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            filtro: '',
-            buscador: ''
-            
-        }
-    }
+  onChange = (e) => {
+    const { value } = e.target;
+    //console.log(e.target.value);
+    this.setState({ buscador: value });
+  };
 
-    onChange = (e) => {
-        const { value } = e.target;
-        //console.log(e.target.value);
-        this.setState({buscador: value});
-    }
-
-    render() {
-        return (
-        <div className="container">
-            <Buscador handChange={this.onChange} />
-            <Temas temas={Data} search={this.state.buscador}/>
-        </div>
-    )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Buscador handChange={this.onChange} />
+        <Temas temas={Data} search={this.state.buscador} />
+      </div>
+    );
+  }
 }
